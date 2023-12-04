e90caf2c070d030e906bafe4bd0eda6c8ee934ae
Piotr Kuncy <kuncypiotr@gmail.com>
2023-12-04 22:40:13 +0100

    tests

--- src/tests/unit/Post.test.tsx
+++ src/tests/unit/Post.test.tsx
@@ -13,3 +13,15 @@ test("post should be in the document and visible", async () => {
   expect(post).toBeInTheDocument()
   expect(post).toBeVisible()
 })
+
+test("post should display 'like' on 1 likes", async () => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: ["/posts/1"],
+    initialIndex: 1
+  })
+
+  render(<RouterProvider router={router} />)
+  var text = await screen.findByText("1 like")
+  expect(text).toBeInTheDocument()
+  expect(text).toBeVisible()
+})
